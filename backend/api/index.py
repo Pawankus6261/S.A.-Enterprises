99dfa699e5040eaf24a7953c9cee2508d62116cb
Pawan Kushwaha <pawankushwaha91719171@gmail.com>
2026-02-03 17:03:31 +0530

    h

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,8 +1,2 @@
-import sys
-import os
-
-# Ye line aapke 'backend' folder ko Python path mein add karti hai
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-# Ab hum aapke main app ko import kar sakte hain
 from backend.main import app
+
